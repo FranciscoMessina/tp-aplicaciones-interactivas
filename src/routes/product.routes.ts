d31f137430c46ac293558a08b4120e8bdadc1fd4
Francisco Messina <francisco@messina.ws>
2026-08-24 20:29:52 -0300

    comentarios

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -13,6 +13,9 @@ const productRouter = Router();
 
 productRouter.get("/", getProducts);
 productRouter.post("/", authenticate, requireAdmin, createProduct);
+// Tiene sentido tener un endpoint especifico para cambiar el estado del producto? O
+// O que se haga directo como una actualización de los datos del mismo, y el front se
+// encarga de mostrarlo como una acción separada? Para mi no tiene sentido que un endpoint extra.
 productRouter.patch("/:id", authenticate, requireAdmin, updateProduct);
 productRouter.delete("/:id", authenticate, requireAdmin, deleteProduct);
 productRouter.patch(
