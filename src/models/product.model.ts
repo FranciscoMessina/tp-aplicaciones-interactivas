78d5f32e4d0792a33609b0286af6f89631c7ac81
FranciscoMessina <francisco@messina.ws>
2026-08-24 12:47:43 -0300

    agrego algunos comentarios de color

--- src/models/product.model.ts
+++ src/models/product.model.ts
@@ -1,15 +1,20 @@
 import { getModelForClass, modelOptions, prop } from "@typegoose/typegoose";
 
+// Seguramente se pregunten, que demonios es esto?
+// Bueno es una forma de definir los modelos de Mongoose con typescript,
+// evita tener codigo extra y duplicado.
+// En este primer decorator se configuran opciones del schema.
 @modelOptions({ schemaOptions: { timestamps: true } })
 export class Product {
-  @prop({ required: true })
+  // En cada prop decorator se configura el campo del documento en si.
+  @prop({ required: true, trim: true })
   public name!: string;
 
-  @prop({ required: true })
+  @prop({ required: true, trim: true })
   public description!: string;
 
-  @prop({ required: true })
-  public quantity!: number;
+  @prop({ required: true, min: 0 })
+  public stock!: number;
 }
 
 export const ProductModel = getModelForClass(Product);
