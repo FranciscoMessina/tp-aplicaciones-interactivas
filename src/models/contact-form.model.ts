3836bf1168e5b1d2f9acc32c2982c794565fc738
FranciscoMessina <francisco@messina.ws>
2026-08-27 15:42:09 -0300

    fix de enum

--- src/models/contact-form.model.ts
+++ src/models/contact-form.model.ts
@@ -1,37 +1,37 @@
 import {
-    getModelForClass,
-    modelOptions,
-    prop,
-    type Ref,
+  getModelForClass,
+  modelOptions,
+  prop,
 } from "@typegoose/typegoose";
 
+export const ContactFormStatus = {
+  Pending: "PENDIENTE",
+  Read: "LEIDA",
+  Resolved: "RESUELTA",
+};
+
+export type ContactFormStatus =
+  (typeof ContactFormStatus)[keyof typeof ContactFormStatus];
+
 @modelOptions({ schemaOptions: { timestamps: true } })
-
-export enum ContactFormStatus {
-    PENDIENTE = "PENDIENTE",
-    LEIDA = "LEIDA",
-    RESUELTA = "RESUELTA",
-}
 export class ContactForm {
+  @prop({ required: true, trim: true })
+  public name!: string;
 
-    @prop({ required: true, trim: true })
-    public name!: string;
+  @prop({ required: true, lowercase: true, trim: true })
+  public email!: string;
 
-    @prop({ required: true, lowercase: true, trim: true })
-    public email!: string;
+  @prop({ trim: true })
+  public phone?: number;
 
-    @prop({ trim: true })
-    public phone?: number;
+  @prop({ required: true, trim: true })
+  public subject!: string;
 
-    @prop({ required: true, trim: true })
-    public subject!: string;
+  @prop({ required: true, trim: true })
+  public message!: string;
 
-    @prop({ required: true, trim: true })
-    public message!: string;
-
-    @prop({ enum: ContactFormStatus, default: ContactFormStatus.PENDIENTE })
-    public status!: ContactFormStatus;
-
+  @prop({ enum: ContactFormStatus, default: ContactFormStatus.Pending })
+  public status!: ContactFormStatus;
 }
 
 export const ContactFormModel = getModelForClass(ContactForm);
